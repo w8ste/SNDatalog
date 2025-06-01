c3028df6e55f7d5559e0d04d7ad59c449a30292b
Hannes <hannes10042004@icloud.com>
2025-06-01 22:47:30 +0200

    Clean up code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,30 +1,26 @@
 import sn.*
 
 @main def hello(): Unit =
+
+  // Predicate definitions
   val parent = Predicate("parent", 2)
   val ancestor = Predicate("ancestor", 2)
-  val reachable = Predicate("reachable", 2)
-  val link = Predicate("linked", 2)
+
+  // edb definitions
   val edb: Map[Predicate, Relation] = Map(
     parent -> Set(
       List("Alice", "Bob"),
       List("Bob", "Charlie"),
       List("Charlie", "Delta")
-    ),
-    link -> Set(
-      List("Alice", "Bob"),
-      List("Bob", "Charlie"),
-      List("Charlie", "Delta")
     ))
 
+  // rule definitions
   val rules = List(
     Rule(Atom(ancestor, List("x", "y")), List(Atom(parent, List("x", "y")))),
     Rule(Atom(ancestor, List("x", "y")), List(Atom(ancestor, List("x", "z")), Atom(parent, List("z", "y")))),
-    Rule(Atom(reachable, List("x", "y")), List(Atom(link, List("x", "y")))),
-    Rule(Atom(reachable, List("x", "y")), List(Atom(reachable, List("x", "z")), Atom(link, List("z", "y"))))
   )
 
-  val result = sn.semiNaiveEvaluation(rules, edb)
+  val result: Map[Predicate, Relation] = sn.semiNaiveEvaluation(rules, edb)
 
   println("--- Derived Facts ---")
   for ((pred, facts) <- result) {
